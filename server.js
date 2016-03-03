a4de5273345d7e0733513ffe5b84f8f937ce1af8
Marybeth Snodgrass <msnodgrass25@gmail.com>
2016-03-03 10:34:16 -0600

    socket.io module on front and back ends

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 'use strict'
 
-const app = require("express")()
+const express= require('express')
+const app = express()
 const server = require('http').createServer(app) //equivalent to express()
-const io = require('socket.io')(server)
+const websocket = require('socket.io')(server)
 
 const PORT = process.env.PORT || 3000
 
+app.set('view engine', 'jade')
+
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    res.send('hello')
+    res.render('index')
 })
 
 server.listen(PORT, () => {
     console.log(`Server listening on port': ${PORT}`)
 })
 
-
+websocket.on('connection', socket => {
+    console.log('connection', socket)
+}) //subscribing to events, client is connecting to node
